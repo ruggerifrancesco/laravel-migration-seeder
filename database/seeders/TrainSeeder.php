68d2b6e6d0cc3d5b3bec3794d2d6767c39901a0e
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-21 16:48:50 +0200

    tested arrays in plus

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -17,12 +17,24 @@ class TrainSeeder extends Seeder
     {
         $testTrainsList = [
             [
-                'company' => 'Trenitalia',
+                'company' => 'Trenitalia2',
                 'departure_station' => 'Lecco',
                 'arrival_station' => 'Monza',
                 'departure_time' => '2023-07-21 10:00:00',
                 'arrival_time' => '2023-07-21 15:30:00',
-                'train_code' => 'f678fefBYHUD',
+                'train_code' => 'f678fefBYrrr',
+                'num_carriages' => 4,
+                'on_time' => true,
+                'canceled' => false,
+                'additional_info' => null,
+            ],
+            [
+                'company' => 'Treno Giapponese',
+                'departure_station' => 'Lecco',
+                'arrival_station' => 'Monza',
+                'departure_time' => '2023-07-21 10:00:00',
+                'arrival_time' => '2023-07-21 15:30:00',
+                'train_code' => 'fewkjfbwek',
                 'num_carriages' => 4,
                 'on_time' => true,
                 'canceled' => false,
@@ -30,19 +42,23 @@ class TrainSeeder extends Seeder
             ],
         ];
 
+        // foreach ($testTrainsList as $train) {
+            // $newTrain = new Train();
+            // $newTrain->company = $train['company'];
+            // $newTrain->departure_station = $train['departure_station'];
+            // $newTrain->arrival_station = $train['arrival_station'];
+            // $newTrain->departure_time = $train['departure_time'];
+            // $newTrain->arrival_time = $train['arrival_time'];
+            // $newTrain->train_code = $train['train_code'];
+            // $newTrain->num_carriages = $train['num_carriages'];
+            // $newTrain->on_time = $train['on_time'];
+            // $newTrain->canceled = $train['canceled'];
+            // $newTrain->additional_info = $train['additional_info'];
+            // $newTrain->save();
+        // }
+
         foreach ($testTrainsList as $train) {
-            $newTrain = new Train();
-            $newTrain->company = $train['company'];
-            $newTrain->departure_station = $train['departure_station'];
-            $newTrain->arrival_station = $train['arrival_station'];
-            $newTrain->departure_time = $train['departure_time'];
-            $newTrain->arrival_time = $train['arrival_time'];
-            $newTrain->train_code = $train['train_code'];
-            $newTrain->num_carriages = $train['num_carriages'];
-            $newTrain->on_time = $train['on_time'];
-            $newTrain->canceled = $train['canceled'];
-            $newTrain->additional_info = $train['additional_info'];
-            $newTrain->save();
+            Train::create($train);
         }
     }
 }
