4b51cf09f193f25685084ead6afdd3399e127a90
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-21 15:38:27 +0200

    created all columns to be accepted in the new database

--- database/migrations/2023_07_21_131036_create_trains_table.php
+++ database/migrations/2023_07_21_131036_create_trains_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company'); // Azienda -> Company (VARCHAR)
+            $table->string('departure_station'); // Stazione di partenza -> Departure Station (VARCHAR)
+            $table->string('arrival_station'); // Stazione di arrivo -> Arrival Station (VARCHAR)
+            $table->dateTime('departure_time'); // Orario di partenza -> Departure Time (DATETIME)
+            $table->dateTime('arrival_time'); // Orario di arrivo -> Arrival Time (DATETIME)
+            $table->string('train_code'); // Codice Treno -> Train Code (VARCHAR)
+            $table->integer('num_carriages'); // Numero Carrozze -> Number of Carriages (INTEGER)
+            $table->boolean('on_time')->default(true); // In orario -> On Time (BOOLEAN)
+            $table->boolean('canceled')->default(false); // Cancellato -> Canceled (BOOLEAN)
+            $table->text('additional_info')->nullable(); // Additional information (TEXT) - Example: for any extra details about the train
             $table->timestamps();
         });
     }
