c72ff619da32926f61c0551e9c538d25df7aa95c
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-21 15:38:15 +0200

    created a new function acalled view home and compacted future trains table db

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function home () {
+        $trains = Train::all();
+        
+        return view('home', compact('trains'));
+    }
 }
