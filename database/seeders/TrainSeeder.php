bc63cde9ad973c3dfd56df29d6c2ebdde6da5d89
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-21 15:48:42 +0200

    tested an array

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -14,6 +14,17 @@ class TrainSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $testTrainsList = [
+            'company' => 'Trenitalia',
+            'departure_station' => 'Lecco',
+            'arrival_station' => 'Monza',
+            'departure_time' => '2023-07-21 10:00:00',
+            'arrival_time' => '2023-07-21 15:30:00',
+            'train_code' => 'f678fefBYHUD',
+            'num_carriages' => 4,
+            'on_time' => true,
+            'canceled' => false,
+            'additional_info' => null,
+        ];
     }
 }
